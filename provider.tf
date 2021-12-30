2ccda94f2c4038787476b6790af2c44cfdbe82ef
Kunal Parekh <kun4lzz@gmail.com>
2021-12-31 00:14:18 +0530

    commiting first git code

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaouminldguaddw4th2rysvt2wezzx2nscn6snhkrc72dn6wjs27gq"
+  user_ocid = "ocid1.user.oc1..aaaaaaaaxnkqvo2dqce2tebfv3nopt4o27g5pgkidkozrr5vhukzl2nvgnzq"
+  fingerprint = "2d:cb:65:c5:ac:49:d2:73:26:81:98:44:ec:98:ec:9c"
+  private_key_path = "/Users/kbparekh/.oci/oci_api_key.pem"
+  region = "ap-mumbai-1"
+}
